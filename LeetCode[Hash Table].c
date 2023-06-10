0184785690c5825c4fc1f5c7187ea6c4690121dc
KshitijS093 <117442572+KshitijS093@users.noreply.github.com>
2023-06-10 12:46:52 +0530

    Add files via upload

---
+++ LeetCode[Hash Table].c
@@ -0,0 +1,34 @@
+class MyHashSet {
+    bool arr[1000001];
+public:
+    MyHashSet() {
+        int i;
+        for(i=0;i<1000001;i++)
+        {
+            arr[i] = false;
+        }
+        
+    }
+    
+    void add(int key) {
+        arr[key] = true;
+        
+    }
+    
+    void remove(int key) {
+        arr[key] = false;
+        
+    }
+    
+    bool contains(int key) {
+        return arr[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
